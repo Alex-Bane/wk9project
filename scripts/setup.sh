11026bcd03fda564f75c48ee622452cde59e4744
Alex-Bane <ABane@QA.com>
2020-12-06 23:48:50 +0000

    fixed setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-git clone https://github.com/Alex-Bane/wk9project.git
 
 cd ./wk9project
-echo $PATH
-sudo /home/jenkins/.local/bin/ansible-playbook -i ansible/inventory ansible/playbook.yaml
+
+sudo ~./.local/bin/ansible-playbook -i ansible/inventory ansible/playbook.yaml

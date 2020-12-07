21c6e5e2058857560fa9eff4aad710abf9420d04
alex-bane <abane@qa.com>
2020-12-07 00:51:44 +0000

    updated setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@
 pwd
 cd ./wk9project
 pwd
-~/.local/bin/ansible-playbook -i ansible/inventory ansible/playbook.yaml
+sudo /home/jenkins/.local/bin/ansible-playbook -i ansible/inventory ansible/playbook.yaml

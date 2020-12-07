f7322fd215de0e7d79d77a629eb7c89740948618
alex-bane <abane@qa.com>
2020-12-07 02:16:40 +0000

    updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ sudo docker pull alexbane/service2:latest
 sudo docker pull alexbane/service3:latest
 sudo docker pull alexbane/service4:latest
 git clone https://github.com/Alex-Bane/wk9project.git
-cd /wk9project
+pwd
 sudo docker stack deploy --compose-file docker-compose.yaml DNDApp
 EOF
